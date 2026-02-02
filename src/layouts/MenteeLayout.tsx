2c36585abd1f725f0d2bc9d8229b1ba37b3d3055
ddddabi <ii887742@gmail.com>
2026-02-02 18:21:27 +0900

    feat: 멘티 화면 플로팅 버튼 추가

--- src/layouts/MenteeLayout.tsx
+++ src/layouts/MenteeLayout.tsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router-dom";
 import AppShell from "@/app/AppShell";
 import MobileHeader from "@/widgets/mo/MobileHeader";
 import MobileBottomNav from "@/widgets/mo/MobileBottomNav";
+import FloatingConsultButton from "@/widgets/floating/FloatingConsultButton";
 
 export default function MenteeLayout() {
   return (
     <AppShell>
       {/* 헤더/메인/네비 3단 고정 레이아웃 */}
-      <div className="grid h-dvh grid-rows-[auto_1fr_auto]">
+      <div className="relative grid h-dvh grid-rows-[auto_1fr_auto]">
         {/* Top (fixed) */}
         <div className="shrink-0 bg-gray-50">
           <MobileHeader />
@@ -17,9 +18,14 @@ export default function MenteeLayout() {
         <main className="overflow-y-auto px-4 pb-6">
           <Outlet />
         </main>
-
+        
         {/* Bottom (fixed) */}
         <div className="shrink-0 bg-gray-50">
+          <FloatingConsultButton
+          to="https://forms.gle/FchKdDcm23JdGHpK9"
+          internal={false}
+          newTab
+        />
           <MobileBottomNav basePath="/mentee" />
         </div>
       </div>
