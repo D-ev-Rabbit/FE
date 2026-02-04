df7decc3d2beffd8907b150c0e4dfecd96ecf90f
ddddabi <ii887742@gmail.com>
2026-02-04 13:55:51 +0900

    feat: 마이페이지, 활동요약 UI 추가

--- src/pages/mentee/SettingsPage.tsx
+++
@@ -1,9 +0,0 @@
-export default function MenteeSettingsPage() {
-  return (
-    <div className="space-y-3">
-      <h1 className="text-xl font-bold">settings</h1>
-
-      {/* 내용 개발 */}
-    </div>
-  );
-}
